05870934d3b5285b596e680f0ab8be5beda0ee5d
riyazgithubavailable <riyazisheikh97@gmail.com>
2024-11-28 12:51:52 +0530

    2nd-commit

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,5 +1,5 @@
-import axios from 'axios';
-import React,{useState,useEffect} from 'react'
+
+import React,{useEffect} from 'react'
 import { addProduct } from '../redux/features/cartSlice';
 import { useDispatch,useSelector } from 'react-redux';
 import { fetchProducts } from '../redux/features/producSlice';
@@ -19,7 +19,7 @@ const Products = () => {
         // }).catch((error) =>{
         //     console.log(error);
         // })
-    },[]);
+    },[dispatch]);
 
           function handleAdd(product) {
             dispatch(addProduct(product))
